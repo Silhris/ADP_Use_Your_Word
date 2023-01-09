aadc2cea079e556a61a15e334e569891956d1a5b
root <root@DESKTOP-09CQDOO>
2023-01-10 00:26:56 +0100

    Add Vuetify

---
+++ vuetify/src/components/UseYourWordCategory.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-container>
+    <v-expansion-panels>
+      <div v-for="element in props.data" :key="element.id" class="p-3">
+        <v-row class="text-center">
+          <v-col cols="12">
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                <label>N°{{element.id}} - {{element.prompt}}</label>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div v-if="props.item == 'subTheTitle'">
+                  StreamingAssets/Content/SubTheTitle/{{element.id}}/{{element.name}}
+                  <video controls width="500" height="500">
+                      <source :src="getVideoPath(element)" type="video/mp4">
+                  </video>
+
+                  <div class="row">
+                    <div class="col-md-3">
+                      <label>Timecode de début</label>
+                      <input v-model="element.start" min="0" />
+                    </div>
+                    <div class="col-md-3">
+                      <label>Timecode de fin</label>
+                      <input v-model="element.end" min="1" />
+                    </div>
+                    <div class="col-md-3">
+                      <label>Position ? wtf</label>
+                      <input v-model="element.position" />
+                    </div>
+                    <div class="col-md-3">
+                      <label>Nom de la vidéo (à voir si on peut renommer les vidéos existantes sinon nom fixe)</label>
+                      <input v-model="element.name" />
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <label class="col-md-12">Liste des propositions</label>
+                  <div v-for="(text, index) in element.houseAnswers" :key="index" class="col-md-3 p-3">
+                    <input v-model="element.houseAnswers[index]" placeholder="edit me" class="form-control" />
+                  </div>
+                </div>
+
+                <div v-if="props.item == 'surveySays_FR'" class="row">
+                  <label class="col-md-12">Liste des propositions 2</label>
+                  <div v-for="(text, index) in element.houseAnswers2" :key="index" class="col-md-3 p-3">
+                    <input v-model="element.houseAnswers2[index]" placeholder="edit me" class="form-control" />
+                  </div>
+                  <label class="col-md-12">Liste des propositions 3</label>
+                  <div v-for="(text, index) in element.houseAnswers3" :key="index" class="col-md-3 p-3">
+                    <input v-model="element.houseAnswers3[index]" placeholder="edit me" class="form-control" />
+                  </div>
+                </div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-col>
+        </v-row>
+      </div>
+    </v-expansion-panels>
+  </v-container>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+  //const fs = require('fs');
+
+
+  const props = defineProps({
+    // TODO: nouvelles vidéos = id supérieur ou égale à l'id 126 (avant, c'est les vidéos du jeu)
+    data: Array,
+    item: String
+  });
+
+  function getVideoPath(element) {
+    return 'StreamingAssets/Content/SubTheTitle/' + element.id + '/' + element.name;
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  iframe {
+  }
+
+  .component {
+    width: 20%;
+    padding: 1rem;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .twitchrow {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
